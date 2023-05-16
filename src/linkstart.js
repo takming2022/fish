eb1324ef0dc346c3d18bd8f0bc8950a489a82c37
LAPTOP-A8VCD1G8\user <D10916142@gs.takming.edu.tw>
2023-05-16 20:39:30 +0800

    更新

--- src/linkstart.js
+++ src/linkstart.js
@@ -6,7 +6,7 @@ const Linkstart = ({ setDemofishtorf,setHistorytorf }) => {
     const history = useNavigate()
     return (
         <div >
-            <input style={{ position: 'fixed', top: '0.1%', left: '32%', width: '600px', zIndex: 1 }}  type={'image'} src='./image/logo.gif'></input>
+            <input style={{ position: 'fixed', top: '0.1%', left: '32%', width: '600px', zIndex: 1 }}  type={'image'} src='./image/logo.png'></input>
             <input style={{ position: 'fixed', top: '30%', left: '39%', width: '400px', zIndex: 11 }} onClick={() => history('/login')} type={'image'} src='./image/linkstart.png'></input>
             <input style={{ position: 'absolute', top: '3%', right: '1%', width: '5%', zIndex: 10 }} onClick={() => setHistorytorf(true)} type={'image'} src='./image/history.png'></input>
         </div>
